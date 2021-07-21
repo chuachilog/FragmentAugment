a32f9982d8d89d5eec1894c22523ebb86b700f09
chuachilog <chuachilog@yahoo.com>
2021-07-21 14:12:43 +0800

    My Vision

--- app/src/main/java/com/example/test/BFragment.kt
+++ app/src/main/java/com/example/test/BFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
@@ -19,8 +20,18 @@ class BFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_b, container, false)
+        val view =  inflater.inflate(R.layout.fragment_b, container, false)
 
+        val tvName :TextView = view.findViewById(R.id.tvName)
+
+//        val name = arguments?.getString("name")
+//        tvName.text = name
+
+        val args = BFragmentArgs.fromBundle(requireArguments())
+
+        tvName.text = args.name
+
+        return view
     }
 
 }
--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.test
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.btnAbout)
+
+        btn.setOnClickListener(){
+
+            val intent = Intent(this, AboutActivity::class.java)
+
+//            val number = Uri.parse("1234567890")
+//            val intent = Intent(Intent.ACTION_DIAL,number)
+
+//            val geo = Uri.parse("geo:3.1390,101.6869")
+//            val intent = Intent(Intent.ACTION_VIEW,geo)
+
+//            val intent = Intent(Intent.ACTION_SEND)
+//            intent.setType("text/plain")
+//            intent.putExtra(Intent.EXTRA_TEXT,"Hello")
+
+            try{
+                startActivity(intent)
+            }catch (e:ActivityNotFoundException){
+                Toast.makeText(this,"App not Found", Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
 }
